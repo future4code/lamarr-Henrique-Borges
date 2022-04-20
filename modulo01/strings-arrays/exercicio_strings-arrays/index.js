b70eabfe635bd770b9922d11a9bba4d170a2139a
HDouglasA <araujohdouglas@gmail.com>
2022-04-19 22:31:59 -0300

    atividade strings-arrays iniciada

---
+++ modulo01/strings-arrays/exercicio_strings-arrays/index.js
@@ -0,0 +1,51 @@
+// Exercícios de interpretação de código:
+
+// Questão 1:
+
+// a) Imprime undefined, pois a variavel não foi declarada.
+
+// b) imprime null, pois a variavel foi declarada como Tal.
+
+// c) imprime a quantidade de intens no array, 11.
+
+// d) imprime 0.
+
+// e)
+
+// f)
+
+// Questão 2: 
+
+// "SUBI NUM ÔNIBUS EM MIRROCOS" 29
+
+// Exercícios de escrita de código:
+
+// Questão 1:
+
+// let nome = prompt("Qual o seu nome?");
+// let email = prompt("Digite seu email:");
+
+// console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
+
+// Questão 2: 
+
+//  let comidasFavoritas = ["Maçã", "Uva", "Melancia", "Tomate", "laranja",]
+
+//  console.log(comidasFavoritas)
+
+//  console.log(`Essas são as minhas comidas preferidas:`)
+//  console.log(comidasFavoritas[0])
+//  console.log(comidasFavoritas[1])
+//  console.log(comidasFavoritas[2])
+//  console.log(comidasFavoritas[3])
+//  console.log(comidasFavoritas[4])
+
+// let favoritaUsuario = prompt("Qual sua comida favorita?")
+
+// console.log()
+
+
+
+
+
+
