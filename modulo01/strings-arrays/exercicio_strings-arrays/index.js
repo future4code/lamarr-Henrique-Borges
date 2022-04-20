eb71ef82878a0d49974d521e55002798afc5c483
HDouglasA <araujohdouglas@gmail.com>
2022-04-20 10:16:36 -0300

    atividade strings-arrays finalizada

--- modulo01/strings-arrays/exercicio_strings-arrays/index.js
+++ modulo01/strings-arrays/exercicio_strings-arrays/index.js
@@ -6,17 +6,19 @@
 
 // b) imprime null, pois a variavel foi declarada como Tal.
 
-// c) imprime a quantidade de intens no array, 11.
+// c) Imprime a quantidade de intens no array, 11.
 
-// d) imprime 0.
+// d) Imprime 3. O i recebe o valoor do indice 0.
 
-// e)
+// e) Está substituindo o valor do indice 1 = [4] por 1 = [19].
 
-// f)
+// f) Imprime 9. O valor do indice 6.
 
 // Questão 2: 
 
-// "SUBI NUM ÔNIBUS EM MIRROCOS" 29
+// "SUBI NUM ÔNIBUS EM MIRROCOS" 29.
+
+// ..........................................................................................................
 
 // Exercícios de escrita de código:
 
@@ -29,20 +31,44 @@
 
 // Questão 2: 
 
-//  let comidasFavoritas = ["Maçã", "Uva", "Melancia", "Tomate", "laranja",]
+// let comidasFavoritas = ["Maçã", "Uva", "Melancia", "Tomate", "laranja",]
 
-//  console.log(comidasFavoritas)
+// console.log(comidasFavoritas)
 
-//  console.log(`Essas são as minhas comidas preferidas:`)
-//  console.log(comidasFavoritas[0])
-//  console.log(comidasFavoritas[1])
-//  console.log(comidasFavoritas[2])
-//  console.log(comidasFavoritas[3])
-//  console.log(comidasFavoritas[4])
+// console.log(`Essas são as minhas comidas preferidas:`)
+// console.log(comidasFavoritas[0])
+// console.log(comidasFavoritas[1])
+// console.log(comidasFavoritas[2])
+// console.log(comidasFavoritas[3])
+// console.log(comidasFavoritas[4])
 
 // let favoritaUsuario = prompt("Qual sua comida favorita?")
 
-// console.log()
+// comidasFavoritas[1] = favoritaUsuario
+
+// console.log(comidasFavoritas)
+
+// Questão 3:
+
+// let listaDeTarefas = []
+
+// let tarefa1 = prompt("Qual sua primeira tarefa do dia?")
+// let tarefa2 = prompt("Qual sua segunda tarefa do dia?")
+// let tarefa3 = prompt("Qual sua terceira tarefa do dia?")
+
+// listaDeTarefas = [tarefa1, tarefa2, tarefa3]
+
+// console.log(listaDeTarefas)
+
+// let tarefaRealizda = prompt("Digite o número da tarefa que já realizou: 0, 1 ou 2")
+
+// listaDeTarefas.splice(tarefaRealizda, 1)
+
+// console.log(listaDeTarefas)
+
+// ..........................................................................................................
+
+
 
 
 
