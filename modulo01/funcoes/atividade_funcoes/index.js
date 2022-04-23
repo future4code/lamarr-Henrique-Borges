882476a2163c2aebb297d887833a8f33958206a2
HDouglasA <araujohdouglas@gmail.com>
2022-04-22 22:16:37 -0300

    Atividade funcoes iniciada

---
+++ modulo01/funcoes/atividade_funcoes/index.js
@@ -0,0 +1,33 @@
+// Exercícios de interpretação de código:
+
+// Questã 1:
+
+// a) 10, 50.
+
+// b) Erro, pois a função está sendo chamada fora do escopo local.
+
+// Questão 2: 
+
+// a) A funçã está recebendo um texto transformando-o em minúsculo 
+//    e verificando se o mesmo coontém a palavra "cenoura" inclusa.
+
+// b) i. = true; 
+//    ii. = true;  
+//    iii. = true.
+
+// ...........................................................................................................
+
+// Exercícios de escrita de código:
+
+// Questão 1: 
+
+// a)
+
+
+// b)
+
+
+// Questão 2: 
+
+// Questão 3: 
+
