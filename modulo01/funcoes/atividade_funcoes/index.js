8b06c2b372f62eebbc51f60f5bd7472e3eb70b32
HDouglasA <araujohdouglas@gmail.com>
2022-04-24 21:43:52 -0300

    Atividade variaveis finalizada

--- modulo01/funcoes/atividade_funcoes/index.js
+++ modulo01/funcoes/atividade_funcoes/index.js
@@ -23,11 +23,112 @@
 
 // a)
 
+// function sobreMim() {
+
+//     "Eu sou Douglas, tenho 23 anos, moro em Caxias e sou estudante."
+
+// }
+
+//     console.log(sobreMim)
+
+// // b) 
+
+// function infoPessoas (nome, idade, cidade, profissao ) {
+    
+//     return `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`
+
+// }
+// console.log(infoPessoas("Douglas", 23, "Caxias", "Estudante") )
+
+// // Questão 2: 
+
+// // a) 
+
+// function somar (num1, num2) {
+    
+//     let soma = num1 + num2
+//     return soma
+// }
+
+// console.log(somar(5, 10))
 
 // b)
 
+// function comparacao (num1, num2) {
+   
+//     let comparar =  num1 > num2
+//     return comparar
+// }
 
-// Questão 2: 
+// console.log(comparacao(5, 10))
+
+// c) 
+
+// function parImpar (num1){
+
+//     let resto = num1 % 2
+//     let par = resto === 0
+
+//     return par
+
+// }
+
+// console.log( parImpar(10) )
+
+// d)
+
+// function mensagem (string) {
+
+//     return string 
+
+// }
+
+// console.log(mensagem("Uma mensagem qualquer").length )
+// console.log( mensagem( "Uma mensagem qualquer".toUpperCase() ) )
 
 // Questão 3: 
+
+// function somar (num1, num2){
+
+//     num1 = Number(prompt("Digite um número"))
+//     num2 = Number(prompt("Digite outro número"))
+
+//     return num1 + num2
+
+// }
+
+// console.log(somar())
+
+//  function subtrair (num1, num2){
+     
+//     num1 = Number(prompt("Digite um número"))
+//     num2 = Number(prompt("Digite outro número"))
+
+//     return num1 - num2
+
+// }
+
+// console.log(subtrair())
+
+// function multiplicar (num1, num2){
+
+//     num1 = Number(prompt("Digite um número"))
+//     num2 = Number(prompt("Digite outro número"))
+
+//     return num1 * num2
+
+// }
+
+// console.log(multiplicar())
+
+//  function dividir (num1, num2){
+
+//     num1 = Number(prompt("Digite um número"))
+//     num2 = Number(prompt("Digite outro número"))
+
+//     return num1 / num2
+
+// }
+
+// console.log(dividir())
 
