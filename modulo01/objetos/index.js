536829b1e9ddc34ead79e2eff7d2fa0c4d9c416f
HDouglasA <araujohdouglas@gmail.com>
2022-04-26 21:58:13 -0300

    atividade objetos iniciada

---
+++ modulo01/objetos/index.js
@@ -0,0 +1,36 @@
+// Exercícios de interpretação de código:
+
+// Questão 1:
+
+// a)
+// 1º console: Vai imprimir o primeiro valor do array elenco: "Matheus Nachtergaele";
+// 2º console: O último valor do array elenco: "Virginia Cavendish";
+// 3º console: O Terceiro valor do array transmissoesHoje: {canal: "Globo", horario: "14h"}.
+
+// Questão 2:
+
+// a) 
+// 1º console: vai imprimir os valores do objeto cachorro;
+// 2º console: vai trocar o nome do animal. De "Juca" para juba;
+// 3º console: vai trocar a letra "a" de "juba" para "o", transformando-o em "jubo".
+
+// b)  Realizar uma cópia do array inteiro, feita essa cópia, podemos manipular ela da maneira
+// que quisermos. Essa sintaxe é chamada de espalhamento ou spread.
+
+// Questão 3:
+
+// a)
+// 1º console: false;
+// 2º console: undefined.
+
+// b) O 1º console vai imprimir o valor atribuído a backender: (false)
+// O 2º imprime undefined, pois não foi criada a propriedade "altura" no objeto "pessoa".
+
+// ...........................................................................................................
+
+// Exercícios de escrita de código: 
+
+// ...........................................................................................................
+
+// Desafios:
+
