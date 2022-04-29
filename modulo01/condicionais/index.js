f2689701b9c7d20d1e64a055903a453b25bc3c5c
HDouglasA <araujohdouglas@gmail.com>
2022-04-28 22:45:01 -0300

    Atividade Condicões iniciada

---
+++ modulo01/condicionais/index.js
@@ -0,0 +1,86 @@
+// Exercícios de interpretação de código:
+
+// Questão 1:
+
+// a) Pede para o usuário inserir um número, e diz se o mesmo é par ou impar.
+// Se for par, passa no teste, se for ímpar não passa.
+
+// b) Números pares.
+
+// c) Números ímpares.
+
+
+// Questão 2:
+
+// a) Pede ao usuário que insira o nome de uma fruta, e diz o valor dela.
+
+// b) 2.25.
+
+// c) O preço da fruta  Pêra  é  R$  5.
+
+
+// Questão 3:
+
+// a) Pedindo ao usuário que insira um número e guardando o valor inserino na variável numero.
+
+// b) se o número inserido for 10: Esse número passou no teste; 
+// (-10): vai acontecer um erro.
+
+// c) A variável mensagem não pôde ser acessada pelo console.log no escopo global, 
+// uma vez que ela pertence ao if (escopo local).
+
+// ...............................................................................................................................
+
+// Exercícios de escrita de código:
+
+// Questão 1:
+
+// let usuario = Number(prompt("Qual a sua idade?"))
+
+// if (usuario >= 18) {
+//     console.log("Você pode dirigir.")
+// }else {
+//     console.log("Você não pode dirigir.")
+// }
+
+// Questão 2:
+
+// let turnoAluno = prompt("Em qual turno você estuda? Digite M para (matutino), V para (Vespertino) ou N para (Noturno")
+
+// if (turnoAluno === "M") {
+//     console.log("Bom Dia!")
+
+// }else if (turnoAluno === "V") {
+//     console.log("Boa Tarde!")
+
+// }else if (turnoAluno === "N") {
+//     console.log("Boa Noite!")
+
+// }else {
+//     console.log("Por favor insira: M, V OU N")
+// }
+
+// Questão 3:
+
+// let turnoAluno = prompt("Em qual turno você estuda? Digite M para (matutino), V para (Vespertino) ou N para (Noturno")
+
+// switch (turnoAluno){
+
+//     case "M":
+//     console.log("Bom Dia!")
+//     break
+
+//     case "V":
+//     console.log("Boa Tarde!")
+//     break
+
+//     case"N":
+//     console.log("Boa Noite!")
+//     break
+
+//     default:
+//     console.log("Por favor insira: M, V OU N")
+//     break
+// }
+
+// Questão 4:
