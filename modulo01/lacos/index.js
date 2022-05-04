44af8dd57a72502b400a100d14b6a765ffb86cd6
HDouglasA <araujohdouglas@gmail.com>
2022-05-03 22:20:45 -0300

    Atividade laços iniciada

---
+++ modulo01/lacos/index.js
@@ -0,0 +1,41 @@
+// Exercícios de interpretação de código:
+
+// Questão 1: O código está acrescentando mais 1 a váriavel i, enquanto ela for menor que 5, 
+// e somando-a com seu valor anterior. resultado final: 10.
+
+// Questão 2:
+
+// a) Vai imprimir todos os números de dentro do array que são maiores que 18. 
+// [19, 21, 23, 25, 27, 30].
+
+// b) Sim, poderia o fazer, removendo o código de if do bloco. 
+// assim o loop passaria por todos os valores(índice) do array.
+
+// Questão 3: O código vai imprimir uma quantidade de asterios até o valor inserido pelo usuáro, 
+// no exemplo valor (4).
+
+// .............................................................................................................
+
+// Exercícios de escrita de código:
+
+// Questão 1: 
+
+// let qtdBichinhos = Number(prompt("Quantos bichinhos de estimação você tem?"))
+// let bichinhos = []
+
+// if (qtdBichinhos === 0) {
+//     console.log("Que pena! Você pode adotar um pet!")
+
+// }else{ 
+
+//     for (i = 0; i < qtdBichinhos; i++) {
+//         nomes = prompt("Qual o nome dele(s)?")
+//         bichinhos.push(nomes)
+
+//     }
+    
+//     console.log(bichinhos)
+// }
+
+// Questão 2:
+
