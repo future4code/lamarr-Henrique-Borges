1ed4ca0ff804263dee44a597c1adeefdd7072b9b
HDouglasA <araujohdouglas@gmail.com>
2022-07-14 18:22:23 -0300

    Atividade thinking-react finalizada

---
+++ modulo03/thinking-react/ecommerce-lbn/src/MockDeDados.js
@@ -0,0 +1,8 @@
+export const produtos = [
+    {id:1, nome:"Produto 01", preco:9, imagem:"", botao:""},
+    {id:2, nome:"Produto 02", preco:18, imagem:"", botao:""},
+    {id:3, nome:"Produto 03", preco:28, imagem:"", botao:""},
+    {id:4, nome:"Produto 04", preco:55, imagem:"", botao:""},
+    {id:5, nome:"Produto 05", preco:123, imagem:"", botao:""},
+    {id:6, nome:"Produto 06", preco:220, imagem:"", botao:""},
+]
