d8db99cc69ca5fff27a45a6d745bf4fd363935a5
HDouglasA <araujohdouglas@gmail.com>
2022-10-07 23:00:13 -0300

    revisao finalizada

--- modulo05/revisao-javascript-backend/exercicio3/index.js
+++ modulo05/revisao-javascript-backend/exercicio3/index.js
@@ -1,1 +1,27 @@
-console.log("exercicio 3");
+// Sistema de login:
+
+const contas = [
+	{
+		email: "astrodev@labenu.com",
+		password: "abc123"
+	},
+	{
+		email: "bananinha@gmail.com",
+		password: "bananinha"
+	}
+]
+
+const cadastrarConta = (emailCadastro, senhaCadastro) => {
+  if(emailCadastro.includes("@") && senhaCadastro.length > 5) {
+   contas.push({email: emailCadastro, password:senhaCadastro})
+   console.log("Login bem-sucedido.");
+  } else {
+    console.log("e-mail ou senha incorretos (senha deve possuir no mínimo 6 caracteres).");
+  } 
+}
+
+console.log(contas);
+
+cadastrarConta("vasco@gmail.com", "o123")
+
+console.log(contas);
