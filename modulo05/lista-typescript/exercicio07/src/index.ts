9365eb9e909917d4d51b43955d3ac9614d71f66b
HDouglasA <araujohdouglas@gmail.com>
2022-10-22 13:45:15 -0300

    atividade 'lista-typescript' finalizada

--- modulo05/lista-typescript/exercicio07/src/index.ts
+++ modulo05/lista-typescript/exercicio07/src/index.ts
@@ -1,1 +1,26 @@
-console.log("vasco");
+type Produto = {
+  nome: string,
+  quantidade: number,
+  valorUnitario: any
+}
+
+const ordenar = (produtos: Produto[]) => {
+
+  const produtosOrdenados = produtos.sort((produtoA, produtoB) => {
+    if (produtoA.quantidade < produtoB.quantidade) return -1;
+    if (produtoA.quantidade > produtoB.quantidade) return 1;
+    return 0;
+  })
+
+  return produtosOrdenados
+}
+
+console.log(ordenar([
+  { nome: "MacMugffin", quantidade: 37, valorUnitario: 51.040 },
+  { nome: "Vassoura voadora", quantidade: 56, valorUnitario: 210.0 },
+  { nome: "Laço da verdade", quantidade: 32, valorUnitario: 571.5 },
+  { nome: "O precioso", quantidade: 1, valorUnitario: 9181.923 },
+  { nome: "Caneta de 250 cores", quantidade: 123, valorUnitario: 17 },
+  { nome: "Plumbus", quantidade: 13, valorUnitario: 140.44 },
+  { nome: "Pokebola", quantidade: 200, valorUnitario: 99.9915 }
+]));
