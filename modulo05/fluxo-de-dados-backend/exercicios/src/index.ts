b94b2687533309b7d1ebdf253396318eab2efd0c
HDouglasA <araujohdouglas@gmail.com>
2022-11-01 22:39:01 -0300

    atividade 'fluxo-de-dados-backend' iniciada

---
+++ modulo05/fluxo-de-dados-backend/exercicios/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/test", (req: Request, res: Response) => {
+  res.send("Servidor rodando na porta 3003!")
+})
+
+app.listen(3003, () => {
+  console.log("servidor rodando na url http://localhost:3003");
+})
