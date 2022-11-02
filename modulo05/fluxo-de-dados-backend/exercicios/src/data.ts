b7669c7089496fc1817515e8dccd64430e53acb5
HDouglasA <araujohdouglas@gmail.com>
2022-11-02 18:48:17 -0300

    atividade 'fluxo-de-dados-backend' finalizada

---
+++ modulo05/fluxo-de-dados-backend/exercicios/src/data.ts
@@ -0,0 +1,19 @@
+import { Produto } from "./type";
+
+export const produtos: Produto[] = [
+  {
+    id: "asdfghjklç",
+    name: "Barril do Chaves",
+    price: 125
+  },
+  {
+    id: "qwertyuiop",
+    name: "Tênis do Sonic",
+    price: 32
+  },
+  {
+    id: "zxcvbnm",
+    name: "Gravata",
+    price: 18
+  }
+]
